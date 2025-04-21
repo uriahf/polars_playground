de7ffacda13c1648ca140dbc3386e68a6c88eda6
Uriah Finkel <Ufinkel@gmail.com>
2025-04-21 18:21:36 +0300

    Play with csv file

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
+import polars as pl
+
 def main():
     print("Hello from polars-playground!")
 
 
 if __name__ == "__main__":
     main()
+
+
+trips = pl.read_csv(
+"data/202403-citibike-tripdata.csv",
+try_parse_dates=True,
+schema_overrides={
+"start_station_id": pl.String,
+"end_station_id": pl.String,
+},
+).sort(
+"started_at"
+)
+
+trips.height
+
+print(trips[:, :4])
+print(trips[:, 4:8])
+print(trips[:, 8:])
+
+
+print(2+2)
